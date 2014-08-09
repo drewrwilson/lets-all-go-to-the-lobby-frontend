49051ba4ec0c82cdb810b9a057201ab3e13105f4
Drew <drew.r.wilson@gmail.com>
2014-08-09 16:05:07 -0700

    load some data, put it in a tabke

--- lets-all-go-to-the-lobby.js
+++ lets-all-go-to-the-lobby.js
@@ -1,7 +1,62 @@
 function loadData () {
 
   d3.json("http://localhost:3000/lobbymoney", function(data) {
+    window.data = data;
     console.log(data);
+
+
+  //the columns that we'd like to display
+  var columns = ['lobbyist_naml','lobbyist_namf'];
+
+  //add data to the html
+  var results = d3.select("#results").html(null),
+        table = results.append("table").attr("class", "tblResults"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
+
+  // append the header row
+  thead.append("tr")
+      .selectAll("th")
+      .data(columns)
+      .enter()
+      .append("th")
+          .text(function(column) { return column; })
+      .on("click", function(d){
+        d3.select('#results table tbody')
+            .selectAll('tr').sort(function(a, b){
+              if (ascending)
+                return d3.ascending(a[d], b[d]);
+              else
+                return d3.descending(a[d], b[d]);
+          });
+          //flip the bit:
+          ascending = !ascending;
+        }
+      );
+
+  //append some data
+    //initialize ascending as true
+  var ascending = true;
+  //add sort icon to the list
+  d3.selectAll('#results table tr th').append('span').html(' <i class="fa fa-sort"></i>')
+
+  // create a row for each object in the data
+  var rows = tbody.selectAll("tr")
+      .data(data)
+      .enter()
+      .append("tr");
+
+  // create a cell in each row for each column
+  var cells = rows.selectAll("td")
+      .data(function(row) {
+          return columns.map(function(column) {
+              return {column: column, value: row[column]};
+          });
+      })
+      .enter()
+      .append("td")
+          .text(function(d) { return d.value; });
   });
+
 
 }
